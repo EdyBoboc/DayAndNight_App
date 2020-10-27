a011f1c540a5cba07483a0c6f80cdf0e50e86fe4
EuardB <edi.boboc33@gmail.com>
2020-10-28 01:57:12 +0200

    Add securitu rooms with C# code ^^

--- DayAndNight/DayAndNight/MainPage.xaml.cs
+++ DayAndNight/DayAndNight/MainPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Essentials;
 using Xamarin.Forms;
+using Xamarin.Forms.PancakeView;
 
 namespace DayAndNight
 {
@@ -14,6 +15,34 @@ namespace DayAndNight
         public MainPage()
         {
             InitializeComponent();
+
+            
+            for(int i = 1; i <= 5; i++)
+            {
+                PancakeView pancake = new PancakeView();
+                pancake.CornerRadius = 10;
+
+                Grid camera = new Grid{ BackgroundColor = Color.Aqua, HeightRequest = 100, WidthRequest = 140 };
+                camera.Children.Add(new Image {
+                    Source = i < 4 ? $"Zona{i}" : $"Zona{i - 2}",
+                    Aspect = Aspect.AspectFill });
+
+                Frame chenar_textCamera = new Frame {
+                    BackgroundColor = Color.FromHex("#ffa418"),
+                    HasShadow = false,
+                    VerticalOptions = LayoutOptions.Start,
+                    HorizontalOptions = LayoutOptions.Start,
+                    Margin = 2,
+                    Padding = new Thickness(8, 2),
+                    CornerRadius = 10
+                };
+
+                chenar_textCamera.Content = new Label { Text = "Zona " + i, TextColor = Color.White };
+
+                camera.Children.Add(chenar_textCamera);
+                pancake.Content = camera;
+                carusel.Children.Add(pancake);
+            }
         }
     }
 }
