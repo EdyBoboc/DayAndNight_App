eab81ebb2b4f95a4058b1a6e2a3db06f405891e5
EuardB <edi.boboc33@gmail.com>
2020-10-29 03:53:35 +0200

    Opțiuni multiple de acțiuni la apăsarea pe poza de profil (adăugare câte o cameră sau schimbare mod interfață)

---
+++ DayAndNight/DayAndNight/Clase/Camere.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DayAndNight.Clase
+{
+    class Camere
+    {
+        public static int NrCamera { get; set; }
+        public string Denumire { get; set; }
+
+        //public Camere() { }
+
+        
+    }
+}
--- DayAndNight/DayAndNight/MainPage.xaml.cs
+++ DayAndNight/DayAndNight/MainPage.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using DayAndNight.Clase;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Xamarin.Essentials;
 using Xamarin.Forms;
@@ -12,14 +14,65 @@ namespace DayAndNight
 {
     public partial class MainPage : ContentPage
     {
+        //Cazuri la apasarea pozei de profil
+        enum Actiuni_PozaProfil
+        {
+            Nimic,
+            AdaugareCamere,
+            SchimbareNightMode
+        }; readonly Actiuni_PozaProfil actiune;
+
+        //Programatic
+        int apasat = 1;
+        bool nightmode = false;
+        BackgroundWorker bw = new BackgroundWorker();
+
         public MainPage()
         {
             InitializeComponent();
 
-            
-            for(int i = 1; i <= 5; i++)
+            //Setare actiune pe poza de profil
+            actiune = Actiuni_PozaProfil.AdaugareCamere;
+
+            //Programatic
+            bw.DoWork += delegate { Thread.Sleep(1300); };
+            bw.RunWorkerCompleted += delegate
             {
                 PancakeView pancake = new PancakeView();
+                pancake.CornerRadius = 10;
+
+                Grid camera = new Grid{ BackgroundColor = Color.Aqua, HeightRequest = 100, WidthRequest = 140 };
+                camera.Children.Add(new Image
+                {
+                    Source = $"Zona{new Random().Next(1, 4)}", //apasat < 4 ? $"Zona{apasat}" : $"Zona{apasat - 2}",
+                    Aspect = Aspect.AspectFill
+                });
+
+                Frame chenar_textCamera = new Frame
+                {
+                    BackgroundColor = Color.FromHex("#ffa418"),
+                    HasShadow = false,
+                    VerticalOptions = LayoutOptions.Start,
+                    HorizontalOptions = LayoutOptions.Start,
+                    Margin = 2,
+                    Padding = new Thickness(8, 2),
+                    CornerRadius = 10
+                };
+
+                chenar_textCamera.Content = new Label { Text = "Zona " + apasat, TextColor = Color.White };
+
+                camera.Children.Add(chenar_textCamera);
+                pancake.Content = camera;
+                carusel.Children.Add(pancake);
+
+                lblCamere.Text = "Camere";
+                apasat++;
+            };
+
+            //nimic
+            /*for(int i = 1; i <= 5; i++)
+            {
+                *//*PancakeView pancake = new PancakeView();
                 pancake.CornerRadius = 10;
 
                 Grid camera = new Grid{ BackgroundColor = Color.Aqua, HeightRequest = 100, WidthRequest = 140 };
@@ -41,7 +94,38 @@ namespace DayAndNight
 
                 camera.Children.Add(chenar_textCamera);
                 pancake.Content = camera;
-                carusel.Children.Add(pancake);
+                carusel.Children.Add(pancake);*//*
+            }*/
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            switch(actiune)
+            {
+                case Actiuni_PozaProfil.Nimic:
+                    break;
+                case Actiuni_PozaProfil.AdaugareCamere:
+                    if(apasat <= 10)
+                    {
+                        lblCamere.Text = "Încărcare cameră...";
+                        if(!bw.IsBusy)
+                            bw.RunWorkerAsync();
+                    }
+                    break;
+                case Actiuni_PozaProfil.SchimbareNightMode:
+                    if(!nightmode)
+                    { 
+                        lblCamere.TextColor = Color.Red;
+                        nightmode = true;
+                    }
+                    else
+                    {
+                        lblCamere.TextColor = Color.FromHex("#213654");
+                        nightmode = false;
+                    }
+                    break;
+                default:
+                    break;
             }
         }
     }
