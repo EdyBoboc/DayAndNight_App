dbcfbe763b59b3e63a2407a0764c5c3d642747cd
EuardB <edi.boboc33@gmail.com>
2020-10-30 19:18:37 +0200

    Adaugare sectiune jos, actiuni pe label-uri pentru adugare dispozitive, camere.

--- DayAndNight/DayAndNight/MainPage.xaml.cs
+++ DayAndNight/DayAndNight/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -18,21 +19,23 @@ namespace DayAndNight
         enum Actiuni_PozaProfil
         {
             Nimic,
-            AdaugareCamere,
+            AdaugareDispozitiv,
+            AdaugareCamera,
             SchimbareNightMode
         }; readonly Actiuni_PozaProfil actiune;
 
         //Programatic
-        int apasat = 1;
+        int nrDispozitive = 1, nrCamere = 1;
         bool nightmode = false;
         BackgroundWorker bw = new BackgroundWorker();
+        BackgroundWorker bwAdaugareCamera = new BackgroundWorker();
 
         public MainPage()
         {
             InitializeComponent();
 
             //Setare actiune pe poza de profil
-            actiune = Actiuni_PozaProfil.AdaugareCamere;
+            //actiune = Actiuni_PozaProfil.AdaugareDispozitiv;
 
             //Programatic
             bw.DoWork += delegate { Thread.Sleep(1300); };
@@ -59,16 +62,57 @@ namespace DayAndNight
                     CornerRadius = 10
                 };
 
-                chenar_textCamera.Content = new Label { Text = "Zona " + apasat, TextColor = Color.White };
+                chenar_textCamera.Content = new Label { Text = "Zona " + nrDispozitive, TextColor = Color.White };
 
                 camera.Children.Add(chenar_textCamera);
                 pancake.Content = camera;
                 carusel.Children.Add(pancake);
 
-                lblCamere.Text = "Camere";
-                apasat++;
+                lblListaDispozitive.Text = "Dispozitive";
+                nrDispozitive++;
             };
 
+
+            bwAdaugareCamera.DoWork += delegate { Thread.Sleep(1300); };
+            bwAdaugareCamera.RunWorkerCompleted += delegate
+            {
+
+                StackLayout camera = new StackLayout{ Orientation = StackOrientation.Vertical };
+
+                BoxView linie = new BoxView
+                {
+                    BackgroundColor = Color.White,
+                    HeightRequest = 2,
+                    WidthRequest = 130
+                };
+
+                Label lblDenumireCamera = new Label
+                {
+                    Margin = new Thickness(10, 0, 0, 0),
+                    TextColor = Color.White,
+                    FontSize = 18,
+                    FontFamily = "TextSemiBold",
+                    Text = "Camera " + nrCamere
+                };
+
+                Label lblNrDispozitiveCamera = new Label
+                {
+                    Margin = new Thickness(10, -10, 0, 0),
+                    TextColor = Color.FromHex("#95a8b6"),
+                    FontSize = 12,
+                    FontFamily = "TextLight",
+                    Text = $"{new Random().Next(2, 6)} dispozitive"
+                };
+
+                camera.Children.Add(linie);
+                camera.Children.Add(lblDenumireCamera);
+                camera.Children.Add(lblNrDispozitiveCamera);
+
+                chenarCamere.Children.Add(camera);
+
+                lblListaCamere.Text = "Camere";
+                nrCamere++;
+            };
             //nimic
             /*for(int i = 1; i <= 5; i++)
             {
@@ -98,35 +142,77 @@ namespace DayAndNight
             }*/
         }
 
+        private void AdaugareCamera(object sender, EventArgs e)
+        {
+            if(nrCamere <= 10)
+            {
+                lblListaCamere.Text = "Adăugare cameră...";
+                if(!bwAdaugareCamera.IsBusy)
+                    bwAdaugareCamera.RunWorkerAsync();
+            }
+        }
+
+        private void AdaugareDispozitiv(object sender, EventArgs e)
+        {
+            if(nrDispozitive <= 10)
+            {
+                lblListaDispozitive.Text = "Încărcare dispozitiv...";
+                if(!bw.IsBusy)
+                    bw.RunWorkerAsync();
+            }
+        }
+
         private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
         {
-            switch(actiune)
+            if(!nightmode)
+            {
+                lblListaDispozitive.TextColor = Color.Red;
+                nightmode = true;
+            }
+            else
+            {
+                lblListaDispozitive.TextColor = Color.FromHex("#213654");
+                nightmode = false;
+            }
+
+            /*switch(actiune)
             {
                 case Actiuni_PozaProfil.Nimic:
                     break;
-                case Actiuni_PozaProfil.AdaugareCamere:
-                    if(apasat <= 10)
+
+                *//* Adaugare dispozitive *//*
+                case Actiuni_PozaProfil.AdaugareDispozitiv:
+                    *//*if(nrDispozitive <= 10)
                     {
-                        lblCamere.Text = "Încărcare cameră...";
+                        lblListaDispozitive.Text = "Încărcare dispozitiv...";
                         if(!bw.IsBusy)
                             bw.RunWorkerAsync();
-                    }
-                    break;
+                    }*//* break;
+
+                *//* Adaugare camera *//*
+                case Actiuni_PozaProfil.AdaugareCamera:
+                    *//*if(nrCamere <= 10)
+                    {
+                        lblListaCamere.Text = "Adăugare cameră...";
+                        if(!bwAdaugareCamera.IsBusy)
+                            bwAdaugareCamera.RunWorkerAsync();
+                    }*//* break;
+
+                *//* Interschimbare interfata *//*
                 case Actiuni_PozaProfil.SchimbareNightMode:
                     if(!nightmode)
-                    { 
-                        lblCamere.TextColor = Color.Red;
+                    {
+                        lblListaDispozitive.TextColor = Color.Red;
                         nightmode = true;
                     }
                     else
                     {
-                        lblCamere.TextColor = Color.FromHex("#213654");
+                        lblListaDispozitive.TextColor = Color.FromHex("#213654");
                         nightmode = false;
-                    }
-                    break;
+                    } break;
                 default:
                     break;
-            }
+            }*/
         }
     }
 }
